4267cd26461bf79f8dcf254fc3bfd8a89d312d3b
FuadBabayev <fuad.babayev19@mail.ru>
2024-01-31 10:25:40 +0400

    some changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+const cssnano = require('gulp-cssnano');
 
-function buildStyles() {
+function buildStyles(){
     return src('sass/**/*.scss')
-        .pipe(sass())
-        .pipe(dest('css'));
+    .pipe(sass())
+    .pipe(purgecss({ content: ['*.html']}))
+    .pipe(cssnano())
+    .pipe(dest('css'))
 }
 
-function watchTask() {
-    watch(['sass/**/*.scss'], buildStyles);
+function watchTask(){
+    watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
